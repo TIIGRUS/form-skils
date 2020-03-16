92a9c56b4f4f149405c3dfe561f2184b79cf2026
TIIGRUS <>
2020-03-16 17:03:19 +0200

    animati to js arrow meter

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,24 @@ import './vendor';
 let count = 0;
 let checked = 0;
 
-function countBoxes(){
-
+function countBoxes() {
+	count = $("input[type='checkbox']").length;
+	// console.log(count);
 }
+
+countBoxes();
+$(':checkbox').click(countBoxes);
+
+function countChecked() {
+	checked = $('input:checked').length;
+	let countStepArrow = parseInt(checked * 1.6 / count * 100, 10);
+	if (countStepArrow > 0) {
+		$('#meter__arrow').attr('transform', `rotate (${countStepArrow} 134 122)`);
+	} else if (countStepArrow === 0) {
+		$('#meter__arrow').attr('transform', 'rotate (-35 134 122)');
+	}
+	$('.skils-level__counter').html(countStepArrow);
+}
+
+countChecked();
+$(':checkbox').click(countChecked);
